23381bc1625aca2782b77a8de8ba4ceee27ae511
Belousova Tatiana <leditekhi@gmail.com>
2025-09-26 22:29:22 +0300

    Using if then else

--- sandbox/src/main/java/Hello.java
+++ sandbox/src/main/java/Hello.java
@@ -1,24 +1,18 @@
-import java.io.File;
-
 public class Hello {
     public static void main(String[] args) {
 
-        try {
-            var z = calculate();
-            System.out.println("Hello, World!");
-        } catch(ArithmeticException exception) {
-            exception.printStackTrace();
-        }
-    }
-        private static int calculate () {
-            var x = 1;
-            var y = 0;
+        var x = 1;
+        var y = 0;
+        if (y == 0) {
+            System.out.println("Division by zero is not allowed");
+        } else {
             var z = divide(x, y);
-            return z;
-        }
-
-        private static int divide ( int x, int y){
-            var z = x / y;
-            return z;
+            System.out.println("Hello, World!");
         }
     }
+
+    private static int divide(int x, int y) {
+        var z = x / y;
+        return z;
+    }
+}
--- sandbox/src/main/java/ru/stqa/geometry/figures/Rectangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Rectangle.java
@@ -1,6 +1,14 @@
 package ru.stqa.geometry.figures;
 
-public record Rectangle(double a, double b) {
+public record Rectangle(
+        double a,
+        double b) {
+
+    public Rectangle {
+        if(a < 0 || b < 0) {
+            throw new IllegalArgumentException("Rectangle side should be non-negative");
+        }
+    }
 
     public static void printArea(Rectangle rectangle) {
         var text = String.format("Площадь прямоугольника со сторонами %f  %f = %f", rectangle.a, rectangle.b, rectangle.area());
