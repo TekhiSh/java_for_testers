2b2daaba3e9d8fa07ce570d4697ceb4434217655
Belousova Tatiana <leditekhi@gmail.com>
2025-10-04 10:30:39 +0300

    update compare conditions

--- sandbox/src/test/java/ru/stqa/geometry/figures/TriangleTests.java
+++ sandbox/src/test/java/ru/stqa/geometry/figures/TriangleTests.java
@@ -56,4 +56,25 @@ public class TriangleTests {
         var t2 = new Triangle(9.0, 5.0, 7.0);
         Assertions.assertEquals(t1, t2);
     }
+
+    @Test
+    void testEquality4() {
+        var t1 = new Triangle(5.0, 7.0, 9.0);
+        var t2 = new Triangle(7.0, 5.0, 9.0);
+        Assertions.assertEquals(t1, t2);
+    }
+
+    @Test
+    void testEquality5() {
+        var t1 = new Triangle(5.0, 7.0, 9.0);
+        var t2 = new Triangle(5.0, 9.0, 7.0);
+        Assertions.assertEquals(t1, t2);
+    }
+
+    @Test
+    void testEquality6() {
+        var t1 = new Triangle(5.0, 7.0, 9.0);
+        var t2 = new Triangle(9.0, 7.0, 5.0);
+        Assertions.assertEquals(t1, t2);
+    }
 }
