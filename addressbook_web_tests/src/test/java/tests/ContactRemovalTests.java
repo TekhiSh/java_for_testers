c0334cdaf6a36068108eccb25990be7823e93e86
Belousova Tatiana <leditekhi@gmail.com>
2025-10-23 22:06:21 +0300

    canRemoveAllContactsAtOnce test added

--- addressbook_web_tests/src/test/java/tests/ContactRemovalTests.java
+++ addressbook_web_tests/src/test/java/tests/ContactRemovalTests.java
@@ -1,6 +1,7 @@
 package tests;
 
 import model.ContactData;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ContactRemovalTests extends TestBase {
@@ -13,5 +14,14 @@ public class ContactRemovalTests extends TestBase {
                     "", "", "", "", "", ""));
         }
         app.contacts().removeContact();
+    }
+
+    @Test
+    public void canRemoveAllContactsAtOnce() {
+        if (app.contacts().getCount() == 0) {
+            app.contacts().createContact(new ContactData().withFirstName("test first name"));
+        }
+        app.contacts().removeAllContacts();
+        Assertions.assertEquals(0, app.contacts().getCount());
     }
 }
