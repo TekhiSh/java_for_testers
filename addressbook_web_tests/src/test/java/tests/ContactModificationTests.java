2b072a17e535a7641fcf3b58a39c590d84172d42
Belousova Tatiana <leditekhi@gmail.com>
2025-10-26 12:58:13 +0200

    Group and contact modification. Homework

---
+++ addressbook_web_tests/src/test/java/tests/ContactModificationTests.java
@@ -0,0 +1,35 @@
+package tests;
+
+import model.ContactData;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Random;
+
+public class ContactModificationTests extends TestBase {
+
+    @Test
+    public void canModifyContact() {
+        if (app.contacts().getCount() == 0) {
+            app.contacts().createContact(new ContactData("", "first", "", "last",
+                    "", "", "", "", "", "",
+                    "", "", "", "", "", ""));
+        }
+        var oldContacts = app.contacts().getList();
+        var rnd = new Random();
+        var index = rnd.nextInt(oldContacts.size());
+        var testContact = new ContactData().withFirstName(randomString(26));
+        app.contacts().modifyContact(oldContacts.get(index), testContact);
+        var newContacts = app.contacts().getList();
+        var expectedList = new ArrayList<>(oldContacts);
+        expectedList.set(index, testContact.withId(oldContacts.get(index).id()));
+        Comparator<ContactData> compareById = (o1, o2) -> {
+            return Integer.compare(Integer.parseInt(o1.id()), Integer.parseInt(o2.id()));
+        };
+        newContacts.sort(compareById);
+        expectedList.sort(compareById);
+        Assertions.assertEquals(newContacts, expectedList);
+    }
+}
