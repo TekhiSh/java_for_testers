d1747353dde744acb8a11260661d5f8f66355e99
Belousova Tatiana <leditekhi@gmail.com>
2025-11-10 08:45:40 +0200

    6.5. Fix corrupted relationships

--- addressbook_web_tests/src/test/java/tests/TestBase.java
+++ addressbook_web_tests/src/test/java/tests/TestBase.java
@@ -1,6 +1,7 @@
 package tests;
 
 import manager.ApplicationManager;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
 import java.io.FileReader;
@@ -18,5 +19,10 @@ public class TestBase {
             app = new ApplicationManager();
             app.init(System.getProperty("browser", "firefox"), properties);
         }
+    }
+
+    @AfterEach
+    void checkDataBaseConsistency() {
+        app.jdbc().checkConsistency();
     }
 }
